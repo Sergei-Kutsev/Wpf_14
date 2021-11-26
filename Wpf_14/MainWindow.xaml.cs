414f02d158b6a6e305fd97e09d5abeec3cb0f9ac
Сергей Куцев <Sergeikutsev34@gmail.com>
2021-11-26 17:36:39 +0300

    Добавьте файлы проекта.

---
+++ Wpf_14/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Wpf_14
+{
+   
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                Name = "Батон",
+                Price = 34.99,
+                ProductType = ProductTypes.Food,
+               ProductImage = "Images/bread.jpg",
+            });
+
+            products.Add(new Product()
+            {
+                Name = "Терафлю",
+                Price = 425.40,
+                ProductType = ProductTypes.Medicine,
+                ProductImage = "Images/teraflu.jpeg",
+            });
+
+            products.Add(new Product()
+            {
+                Name = "Молоко",
+                Price = 64.99,
+                ProductType = ProductTypes.Food,
+                ProductImage = "Images/milk.jpg",
+            });
+
+
+            products.Add(new Product()
+            {
+                Name = "Нурофен",
+                Price = 253.70,
+                ProductType = ProductTypes.Medicine,
+                ProductImage = "Images/Nurofen.jpeg",
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Wpf_14/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_14
+{
+    public enum ProductTypes
+    {
+        Food,
+        Medicine
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ProductImage { get; set; }
+        public ProductTypes ProductType { get; set; }
+    }
+}
